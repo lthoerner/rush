fc4d1ad7e0aea9d6c0c898c6e83c129995e90d19
Lowell Thoerner <lowellashton@gmail.com>
2023-03-26 11:57:50 -0700

    Added basic prompt/response mechanism.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io::{stdin, stdout, Write};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let line = prompt();
+        print!("{}", line);
+    }
+}
+
+fn prompt() -> String {
+    print!("rush > ");
+    flush();
+    read_line()
+}
+
+fn flush() {
+    let mut stdout = stdout();
+    stdout.flush().expect("Failed to flush");
+}
+
+fn read_line() -> String {
+    let mut line = String::new();
+    let stdin = stdin();
+    stdin.read_line(&mut line).expect("Failed to read line");
+    
+    line
 }
