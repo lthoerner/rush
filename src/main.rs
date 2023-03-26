8279b5dc307d6fe5dafb16b30bb43dd25b2c8ce2
Lowell Thoerner <lowellashton@gmail.com>
2023-03-26 12:47:01 -0700

    Added user and path resolution.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
 use std::io::{stdin, stdout, Write};
+use std::env::var;
+
+use colored::Colorize;
 
 fn main() {
+    let user = get_current_user();
+    let path = get_current_path();
+
     loop {
-        let line = prompt();
+        let line = prompt(&user, &path);
         print!("{}", line);
     }
 }
 
-fn prompt() -> String {
-    print!("rush > ");
+fn get_current_user() -> String {
+    var("USER").expect("Failed to get user")
+}
+
+fn get_current_path() -> String {
+    var("PWD").expect("Failed to get path")
+}
+
+fn prompt(user: &String, path: &String) -> String {
+    print!("{} on {} > ", user.blue(), path.green());
     flush();
     read_line()
 }
@@ -22,6 +36,6 @@ fn read_line() -> String {
     let mut line = String::new();
     let stdin = stdin();
     stdin.read_line(&mut line).expect("Failed to read line");
-    
+
     line
 }
