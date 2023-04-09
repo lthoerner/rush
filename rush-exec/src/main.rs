49477994b6aaa4c2405fc63858a98de9f6003067
Lowell Thoerner <lowellashton@gmail.com>
2023-04-08 23:08:44 -0700

    Added binary crate rush-exec to handle the actuall execution of commands due to circular dependency conflict of using rush-shell as the binary crate.

--- rush-exec/src/main.rs
+++ rush-exec/src/main.rs
@@ -1,21 +1,13 @@
-mod builtins;
-mod commands;
-mod environment;
-mod errors;
-mod path;
-mod shell;
-
 use anyhow::Result;
 
-use shell::Shell;
-
-use rush_repl::prompt::Repl;
+use rush_shell::shell::Shell;
+// use rush_repl::prompt::Repl;
 
 // TODO: Add upstream error handling here
 fn main() -> Result<()> {
     let mut shell = Shell::new()?;
     shell.run()
 
-    // let repl = Repl::new();
+    // let mut repl = Repl::new();
     // repl.run()
 }
---
+++ rush-shell/src/lib.rs
@@ -0,0 +1,8 @@
+#![allow(dead_code, unused_imports)]
+
+mod builtins;
+mod commands;
+mod environment;
+mod errors;
+mod path;
+pub mod shell;
