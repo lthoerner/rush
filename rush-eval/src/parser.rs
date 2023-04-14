eac7807b99a2d83eb2083229f8b18a1141d30fc1
MitchWijt <mitchel@wijt.net>
2023-04-14 15:02:43 +0200

    feat: rough first implementation of tokenizer

--- rush-eval/src/parser.rs
+++ rush-eval/src/parser.rs
@@ -1,40 +1,86 @@
-// Splits arguments by spaces, taking quotes into account
-// $ This is a temporary solution, and will be replaced by a proper parser
-pub fn tokenize(line: &String) -> (String, Vec<String>) {
-    let line = line.trim();
-    let mut args = Vec::new();
-    let mut current_arg = String::new();
-    let mut in_quotes = false;
 
-    for c in line.chars() {
-        match c {
-            '"' => {
-                if in_quotes {
-                    args.push(current_arg);
-                    current_arg = String::new();
-                }
+// lexical scanner
+// tokenize input
+// parse tokens and create AST
 
-                in_quotes = !in_quotes;
-            }
-            ' ' => {
-                if in_quotes {
-                    current_arg.push(c);
-                } else {
-                    args.push(current_arg);
-                    current_arg = String::new();
+
+// use std::str::Chars;
+fn tokenizer(input: &String) -> Vec<String> {
+    let mut curr_token = String::new();
+    let mut tokens: Vec<String> = Vec::new();
+
+    let mut characters = input.trim().chars().peekable();
+    let mut in_double_quotes = false;
+    let mut in_single_quotes = false;
+
+    loop {
+        let character = characters.next();
+        // handle when a single quote is encountered
+        // handle when a double quote is encountered
+        // handle when a \ is encountered
+        // handle when a \0 a \e or a \x is encountered (most likely the start of a ANSII sequence which is also between double qoutes
+        // handle \r, \t and \n as separate tokens
+        // ANSI sequences can be handled as separate tokens
+
+        match character {
+            Some(v) => {
+                match v {
+                    '\'' => {
+                        in_single_quotes = !in_single_quotes;
+                    }
+                    '"' => {
+                        in_double_quotes = !in_double_quotes;
+                    }
+                    '\\' => {
+                        match characters.peek() {
+                            Some(peeked_char) => {
+                                if peeked_char == &'n' {
+                                    if in_single_quotes || in_double_quotes {
+                                        // clear token and push the newline and backslash into the token
+                                        tokens.push(curr_token.clone());
+                                        curr_token.clear();
+
+                                        curr_token.push(v);
+                                        curr_token.push(*peeked_char);
+                                        characters.next();
+
+                                        tokens.push(curr_token.clone());
+                                        curr_token.clear();
+                                    } else {
+                                        continue;
+                                    }
+                                }
+                            },
+                            None => {
+                                continue;
+                            }
+                        }
+                    },
+                    ' ' => {
+                        if in_single_quotes || in_double_quotes {
+                            curr_token.push(v)
+                        } else if !curr_token.is_empty() {
+                            tokens.push(curr_token.clone());
+                            curr_token.clear();
+                        }
+                    },
+                    _ => curr_token.push(v)
                 }
+            },
+            None => {
+                tokens.push(curr_token.clone());
+                curr_token.clear();
+                break;
             }
-            _ => current_arg.push(c),
         }
     }
 
-    if args.is_empty() {
-        return (current_arg, Vec::new());
-    }
-
-    if !current_arg.is_empty() {
-        args.push(current_arg);
-    }
-
-    (args.remove(0), args)
+    println!("{:?}", tokens);
+    return tokens
 }
+
+pub fn tokenize(input: &String) -> (String, Vec<String>) {
+    tokenizer(input);
+    return (String::from("test"), vec![String::from("testArg")]);
+}
+
