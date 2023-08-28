df4eea52bb8be1c9b8ab22864f33af5391527afc
doinkythederp <doinkythederp@icloud.com>
2023-08-28 11:12:13 -0700

    refactor(plugins): improve method names

--- src/plugins/memory.rs
+++ src/plugins/memory.rs
@@ -1,7 +1,14 @@
+use thiserror::Error;
+use wasmtime::Val;
+
 pub mod manager;
 
 /// The address of a byte that may be controlled by the WASM engine.
 pub type WasmPtr = u32;
+
+#[derive(Debug, Error)]
+#[error("Val must be an i64 to convert to a WasmSpan")]
+pub struct TryFromWasmError;
 
 /// A span of bytes that may be controlled by the WASM engine.
 /// This can be passed to a [`WasmMemoryManager`]
@@ -13,17 +20,22 @@ pub struct WasmSpan {
 }
 
 impl WasmSpan {
-    /// Returns an i64 where the first half is the offset and the 2nd is the length
-    pub fn as_wide_pointer(&self) -> i64 {
+    /// Returns an i64 where the first half is the offset of the memory and the 2nd is its length
+    pub fn to_wasm(&self) -> Val {
+        // For the plugin to use our data, it needs to know where to find
+        // it and how long it is. In this implementation, we represent buffers as
+        // a 64 bit number where the first half is the pointer to the buffer
+        // and the second half is its length.
         let ptr = (self.offset as u64) << 32 | (self.length as u64);
-        ptr as i64
+        Val::I64(ptr as i64)
     }
 
     /// Parses an i64 where the first half is the offset and the 2nd is the length
-    pub fn from_wide_pointer(ptr: i64) -> Self {
+    pub fn try_from_wasm(ptr: &Val) -> Result<Self, TryFromWasmError> {
+        let ptr = ptr.i64().ok_or(TryFromWasmError)? as u64;
         let offset = (ptr >> 32) as u32;
         let length = (ptr & 0xFFFFFFFF) as u32;
-        Self { offset, length }
+        Ok(Self { offset, length })
     }
 }
 
@@ -37,7 +49,7 @@ mod tests {
             offset: u32::MAX,
             length: u32::MAX - 1,
         };
-        let ptr = span.as_wide_pointer();
-        assert_eq!(WasmSpan::from_wide_pointer(ptr), span);
+        let ptr = span.to_wasm();
+        assert_eq!(WasmSpan::try_from_wasm(&ptr).unwrap(), span);
     }
 }
