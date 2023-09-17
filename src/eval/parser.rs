d67bb57e62f6ba1f1e30eb72d4361250bd36d4da
lthoerner <lowellashton@gmail.com>
2023-09-17 16:01:34 -0700

    feat(eval): add commands to parse in test cases

--- src/eval/parser.rs
+++ src/eval/parser.rs
@@ -30,23 +30,129 @@ mod test {
     use super::*;
 
     #[test]
-    fn tokenizer_test_1() {
-        let cmd = "do the thing || ping pong | grep \"response and xyz\" | foo \'bar baz\'";
-        let parse_result = RushTokenizer::parse(Rule::input_line, cmd);
-        println!("{:#?}", parse_result);
+    fn simple_command_no_quotes() {
+        let command = r#"echo Hello world!"#;
+        let parse_result = RushTokenizer::parse(Rule::input_line, command);
     }
 
     #[test]
-    fn tokenizer_test_2() {
-        let cmd = "ping (getip \'google website\') | grep response";
-        let parse_result = RushTokenizer::parse(Rule::input_line, cmd);
-        println!("{:#?}", parse_result);
+    fn simple_command_double_quotes() {
+        let command = r#"echo "Hello world!""#;
+        let parse_result = RushTokenizer::parse(Rule::input_line, command);
     }
 
     #[test]
-    fn tokenizer_test_3() {
-        let cmd = "test 1234";
-        let parse_result = RushTokenizer::parse(Rule::input_line, cmd);
-        println!("{:#?}", parse_result);
+    fn simple_command_single_quotes() {
+        let command = r#"echo 'Hello world!'"#;
+        let parse_result = RushTokenizer::parse(Rule::input_line, command);
+    }
+
+    #[test]
+    fn simple_command_escapes() {
+        let command = r#"echo Hello\ world!"#;
+        let parse_result = RushTokenizer::parse(Rule::input_line, command);
+    }
+
+    #[test]
+    fn simple_command_substitution() {
+        let command = r#"echo (date)"#;
+        let parse_result = RushTokenizer::parse(Rule::input_line, command);
+    }
+
+    #[test]
+    fn single_pipe() {
+        let command = r#"ls | sort"#;
+        let parse_result = RushTokenizer::parse(Rule::input_line, command);
+    }
+
+    #[test]
+    fn multi_pipe() {
+        let command = r#"ls | sort | grep rwx"#;
+        let parse_result = RushTokenizer::parse(Rule::input_line, command);
+    }
+
+    #[test]
+    fn single_logic_operation() {
+        let command = r#"mkdir testdir && cd testdir"#;
+        let parse_result = RushTokenizer::parse(Rule::input_line, command);
+    }
+
+    #[test]
+    fn multi_logic_operation() {
+        let command = r#"mkdir testdir && cd testdir && touch testfile"#;
+        let parse_result = RushTokenizer::parse(Rule::input_line, command);
+    }
+
+    #[test]
+    fn single_redirect() {
+        let command = r#"sort < file.txt"#;
+        let parse_result = RushTokenizer::parse(Rule::input_line, command);
+    }
+
+    #[test]
+    fn multi_redirect() {
+        let command = r#"sort < unsorted.txt >> sorted.txt"#;
+        let parse_result = RushTokenizer::parse(Rule::input_line, command);
+    }
+
+    #[test]
+    fn special_chars() {
+        // TODO: Figure out whether EOF should be part of syntax
+        let command = r#"echo \nHello\ world!\nEOF"#;
+        let parse_result = RushTokenizer::parse(Rule::input_line, command);
+    }
+
+    #[test]
+    fn nested_substitution() {
+        let command = r#"echo (echo (date))"#;
+        let parse_result = RushTokenizer::parse(Rule::input_line, command);
+    }
+
+    #[test]
+    fn nested_substitution_with_pipe() {
+        let command = r#"echo (ls | wc -l)"#;
+        let parse_result = RushTokenizer::parse(Rule::input_line, command);
+    }
+
+    #[test]
+    fn logic_operation_with_substitution() {
+        let command = r#"test -d testdir && echo "(date): Directory exists"#;
+        let parse_result = RushTokenizer::parse(Rule::input_line, command);
+    }
+
+    #[test]
+    fn logic_operation_with_redirect() {
+        let command = r#"ls fake_dir 2>/dev/null || echo "(date): Directory does not exist"#;
+        let parse_result = RushTokenizer::parse(Rule::input_line, command);
+    }
+
+    #[test]
+    fn double_sided_redirect() {
+        let command = r#"command <> file.txt"#;
+        let parse_result = RushTokenizer::parse(Rule::input_line, command);
+    }
+
+    #[test]
+    fn pattern_matching() {
+        let command = r#"ls *.txt"#;
+        let parse_result = RushTokenizer::parse(Rule::input_line, command);
+    }
+
+    #[test]
+    fn multi_argument() {
+        let command = r#"command --option argument"#;
+        let parse_result = RushTokenizer::parse(Rule::input_line, command);
+    }
+
+    #[test]
+    fn double_quotes_with_escapes() {
+        let command = r#"echo "This is a \"test\""#;
+        let parse_result = RushTokenizer::parse(Rule::input_line, command);
+    }
+
+    #[test]
+    fn double_quotes_with_special_chars() {
+        let command = r#"echo -e "First line\nSecond line"#;
+        let parse_result = RushTokenizer::parse(Rule::input_line, command);
     }
 }
